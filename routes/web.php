ea8704d872b255769cfc9b1d0852804b3d9f00bc
localdisk <smoochynet@gmail.com>
2020-08-15 20:20:17 +0900

    remove auth files.

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'top')->name('top');
 
-Route::group(['middleware' => 'guest'], function () {
-    Route::livewire('/register', 'register')->name('register');
-    Route::livewire('/login', 'login')->name('login');
-    Route::livewire('/password/reset', 'password-reset-request-form')->name('password.request');
-    Route::livewire('/password/reset/{token}', 'password-reset-form')->name('password.reset');
-});
-
-Route::group(['middleware' => ['auth', 'verified']], function () {
-    Route::view('/home', 'home')->name('home');
-});
+Route::view('/home', 'home')->name('home');
