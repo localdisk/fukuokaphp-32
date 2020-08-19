3c83749177c04c3c5d2d9e1a3b5894e0550b0eed
localdisk <smoochynet@gmail.com>
2020-08-19 09:23:12 +0900

    fix psalm

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,8 +48,8 @@ class Handler extends ExceptionHandler
      *
      * @throws \Throwable
      */
-    public function render($request, Throwable $exception)
+    public function render($request, Throwable $e)
     {
-        return parent::render($request, $exception);
+        return parent::render($request, $e);
     }
 }
